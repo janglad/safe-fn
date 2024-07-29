13870d939c279c7de8b6075c60d9b2c6e295b579
janglad <jan.gladines@gmail.com>
2024-07-29 16:43:09 +0200

    add action fn type tests

--- src/safe-fn.test-d.ts
+++ src/safe-fn.test-d.ts
@@ -65,3 +65,121 @@ describe("output", () => {
     expectTypeOf(safeFn._outputSchema).toMatchTypeOf<typeof outputSchema>();
   });
 });
+
+describe("action", () => {
+  describe("input", () => {
+    test("should type parsed input as never without input schema"),
+      () => {
+        const safeFn = SafeFn.new();
+        type ActionFn = Parameters<typeof safeFn.action>[0];
+        type ActionFnArgs = Parameters<ActionFn>[0];
+
+        expectTypeOf<ActionFnArgs["parsedInput"]>().toMatchTypeOf<never>();
+      };
+    test("should type parsed input as inputSchema for primitives ", () => {
+      const inputSchema = z.string();
+      const safeFn = SafeFn.new().input(inputSchema);
+
+      type ActionFn = Parameters<typeof safeFn.action>[0];
+      type ActionFnArgs = Parameters<ActionFn>[0];
+
+      expectTypeOf<ActionFnArgs["parsedInput"]>().toMatchTypeOf<
+        z.output<typeof inputSchema>
+      >();
+    });
+
+    test("should type parsed input as inputSchema for objects", () => {
+      const inputSchema = z.object({
+        test: z.string(),
+        nested: z.object({
+          value: z.number(),
+        }),
+      });
+      const safeFn = SafeFn.new().input(inputSchema);
+
+      type ActionFn = Parameters<typeof safeFn.action>[0];
+      type ActionFnArgs = Parameters<ActionFn>[0];
+
+      expectTypeOf<ActionFnArgs["parsedInput"]>().toMatchTypeOf<
+        z.output<typeof inputSchema>
+      >();
+    });
+
+    test("should type parsed input as inputSchema for transformed schemas", () => {
+      const inputSchema = z
+        .object({
+          test: z.string(),
+          nested: z.object({
+            value: z.number(),
+          }),
+        })
+        .transform(({ test }) => ({ test }));
+      const safeFn = SafeFn.new().input(inputSchema);
+
+      type ActionFn = Parameters<typeof safeFn.action>[0];
+      type ActionFnArgs = Parameters<ActionFn>[0];
+
+      expectTypeOf<ActionFnArgs["parsedInput"]>().toEqualTypeOf<
+        z.input<typeof inputSchema>
+      >();
+    });
+  });
+
+  describe("output", () => {
+    test("should type output as any without output schema", () => {
+      const safeFn = SafeFn.new();
+      type ActionFn = Parameters<typeof safeFn.action>[0];
+      type ActionFnReturn = Awaited<ReturnType<ActionFn>>;
+
+      expectTypeOf<ActionFnReturn>().toEqualTypeOf<any>();
+    });
+
+    test("should type output as outputSchema for primitives", () => {
+      const outputSchema = z.string();
+      const safeFn = SafeFn.new().output(outputSchema);
+
+      type ActionFn = Parameters<typeof safeFn.action>[0];
+      type ActionFnReturn = Awaited<ReturnType<ActionFn>>;
+
+      expectTypeOf<ActionFnReturn>().toMatchTypeOf<
+        z.input<typeof outputSchema>
+      >();
+    });
+
+    test("should type output as outputSchema for objects", () => {
+      const outputSchema = z.object({
+        test: z.string(),
+        nested: z.object({
+          value: z.number(),
+        }),
+      });
+      const safeFn = SafeFn.new().output(outputSchema);
+
+      type ActionFn = Parameters<typeof safeFn.action>[0];
+      type ActionFnReturn = Awaited<ReturnType<ActionFn>>;
+
+      expectTypeOf<ActionFnReturn>().toMatchTypeOf<
+        z.infer<typeof outputSchema>
+      >();
+    });
+
+    test("should type output as outputSchema for transformed schemas", () => {
+      const outputSchema = z
+        .object({
+          test: z.string(),
+          nested: z.object({
+            value: z.number(),
+          }),
+        })
+        .transform(({ test }) => ({ test }));
+      const safeFn = SafeFn.new().output(outputSchema);
+
+      type ActionFn = Parameters<typeof safeFn.action>[0];
+      type ActionFnReturn = Awaited<ReturnType<ActionFn>>;
+
+      expectTypeOf<ActionFnReturn>().toMatchTypeOf<
+        z.infer<typeof outputSchema>
+      >();
+    });
+  });
+});
