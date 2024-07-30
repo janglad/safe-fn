1a31d27c17008b5aabe5c97fae5c2845e148e739
janglad <jan.gladines@gmail.com>
2024-07-30 11:11:19 +0200

    switch result types

--- src/result.ts
+++ src/result.ts
@@ -1,23 +1,21 @@
-export type Result<TData, TError> =
-  | {
-      success: true;
-      data: TData;
-      error: never;
-    }
-  | {
-      success: false;
-      error: TError;
-      data: never;
-    };
+export type Result<TData, TError> = Ok<TData> | Err<TError>;
 
-export type Ok<TData> = Result<TData, never>;
+export type Ok<TData> = {
+  success: true;
+  data: TData;
+  error: never;
+};
 export const Ok = <TData>(data: TData): Ok<TData> => ({
   success: true,
   data,
   error: undefined as never,
 });
 
-export type Err<TError> = Result<never, TError>;
+export type Err<TError> = {
+  success: false;
+  error: TError;
+  data: never;
+};
 export type AnyErr = Err<any>;
 export const Err = <TError>(error: TError): Err<TError> => ({
   success: false,
