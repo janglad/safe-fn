ef410883813a4f71527c6ad0b045fef0f7664c5b
janglad <jan.gladines@gmail.com>
2024-09-12 11:57:46 +0300

    return ResultAsync from hook

--- packages/safe-fn-react/src/useServerAction.ts
+++ packages/safe-fn-react/src/useServerAction.ts
@@ -1,6 +1,8 @@
+import { ResultAsync } from "neverthrow";
 import { useRef, useState, useTransition } from "react";
 import {
   actionResultToResult,
+  type ActionResultPromiseToResultAsync,
   type ActionResultToResult,
   type AnySafeFnAction,
   type InferSafeFnActionArgs,
@@ -9,25 +11,30 @@ import {
 
 type UseServerActionReturn<
   TAction extends AnySafeFnAction,
+  THotActionActionResult = InferSafeFnActionReturn<TAction>,
   // Original `ActionResult<T,E>`
-  TActionActionResult = Awaited<InferSafeFnActionReturn<TAction>>,
+  TActionActionResult = Awaited<THotActionActionResult>,
   // Converted `ActionResult<T,E>` -> `Result<T,E>` to be returned to the user
   TActionResult = ActionResultToResult<TActionActionResult>,
+  TActionResultAsync = ActionResultPromiseToResultAsync<THotActionActionResult>,
 > = {
   isPending: boolean;
   isSuccess: boolean;
   result: TActionResult | undefined;
-  execute: (args: InferSafeFnActionArgs<TAction>) => Promise<TActionResult>;
+  execute: (args: InferSafeFnActionArgs<TAction>) => TActionResultAsync;
 };
 
 export const useServerAction = <TAction extends AnySafeFnAction>(
   action: TAction,
 ): UseServerActionReturn<TAction> => {
   type ActionArgs = InferSafeFnActionArgs<TAction>;
+  type THotActionActionResult = InferSafeFnActionReturn<TAction>;
   /** Original `ActionResult<T,E>` */
-  type ActionActionResult = Awaited<InferSafeFnActionReturn<TAction>>;
+  type ActionActionResult = Awaited<THotActionActionResult>;
   /** Converted `ActionResult<T,E>` -\> `Result<T,E>` to be returned to the user  */
   type ActionResult = ActionResultToResult<ActionActionResult>;
+  type ActionResultAsync =
+    ActionResultPromiseToResultAsync<THotActionActionResult>;
 
   const [result, setResult] = useState<ActionResult | undefined>(undefined);
   const [isExecuting, setIsExecuting] = useState(false);
@@ -45,17 +52,19 @@ export const useServerAction = <TAction extends AnySafeFnAction>(
     setIsExecuting(false);
   };
 
-  const execute = async (args: ActionArgs): Promise<ActionResult> => {
-    return new Promise((resolve) => {
+  const execute = (args: ActionArgs): ActionResultAsync => {
+    const promise = new Promise((resolve) => {
       setIsExecuting(true);
       startTransition(() => {
         resolveRef.current = resolve;
-        _execute(args).catch((e: unknown) => {
-          // TODO: Handle error
-          throw e;
-        });
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        _execute(args);
       });
     }) as Promise<ActionResult>;
+    return ResultAsync.fromPromise(
+      promise,
+      () => new Error("Unknown error"),
+    ).andThen((res) => res) as ActionResultAsync;
   };
 
   return {
